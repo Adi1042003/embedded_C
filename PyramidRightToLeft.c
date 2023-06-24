7259632ce471ee82c50fd61edbbd27bd1373380d
Aditya_Puri <101449469+Adi1042003@users.noreply.github.com>
2023-06-24 18:56:10 +0530

    Create PyramidRightToLeft.c

---
+++ PyramidRightToLeft.c
@@ -0,0 +1,30 @@
+/* pattern to display
+                        *
+                       **
+                      ***
+                     ****
+                    *****
+*/
+#include <LPC214x.h>  
+int i;
+int delay (unsigned int time)
+{
+	int x,y;
+	for(x=0;x<=time;x++)
+	for(y=0;y<=1275;y++);//generate 1 micro sec ka delay
+	return 0;
+}	  
+int main() {
+  PINSEL0 = 0x0000;//select P0.0 - P0.15
+  IO0DIR |= 0xFF;  
+
+	for(i=0;i<=7;i++)
+	{
+		IO0CLR|=0XFF00;
+		IO0SET|=(1<<i);//moving from right to left 
+		delay(2000);
+	}
+	IO0CLR|=0XFFFF;	
+}
+
+
