2eedbe3c555d9dbbf09d5e230f7b1ac6498cc10c
Aditya_Puri <101449469+Adi1042003@users.noreply.github.com>
2023-06-29 15:49:17 +0530

    Create FirstIAque4th.c

---
+++ FirstIAque4th.c
@@ -0,0 +1,44 @@
+#include <LPC214x.h>
+
+int delay(unsigned int time)
+{
+    int x, y;
+    for (x = 0; x < time; x++) {
+        for (y = 0; y < 1275; y++); // generate 1 microsec ka delay
+    }
+    return 0;
+}
+
+unsigned int Display[] = {
+    0x3F00,
+    0x0600,
+    0x5B00,
+    0x4F00,
+    0x6600,
+    0x6D00,
+    0x7D00,
+    0x0700,
+    0x7F00,
+    0x6F00
+};
+
+int main() {
+    PINSEL0 = 0x00000000; // select P0.0 - P0.15
+    PINSEL1 = 0x00000000;
+    IO0DIR |= 0xFF00;// output from P0.8 - P0.15
+    IO1DIR |= (1 << 9); //OUTPUT PIN FOR BUZZER
+
+    while (1) {
+        for (i = 0; i < 10; i++) {
+            delay(100);
+            IO0SET = Display[i];
+            delay(1000);
+            IO0CLR = Display[i];
+            if (i == 9) {
+                IO1SET = (1 << 9); // buzzer
+                delay(1000);
+                IO1CLR = (1 << 9);
+            }
+        }
+    }
+}
