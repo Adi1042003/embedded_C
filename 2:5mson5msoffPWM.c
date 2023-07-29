a6a82bb246cdac122021529aa11815fb2220ea1a
Aditya_Puri <101449469+Adi1042003@users.noreply.github.com>
2023-07-29 16:01:05 +0530

    Create 2:5mson5msoffPWM.c
    5. Generate a wave form for Ton= 2.5 m sec and Toff= 5 m sec with a frequency of 1KHz.

---
+++ 2:5mson5msoffPWM.c
@@ -0,0 +1,41 @@
+//5. Generate a wave form for Ton= 2.5 m sec and Toff= 5 m sec with a frequency of 1KHz.
+
+#include"lpc214x.h"
+
+void PLL_init(void);
+void PWM_init(void);
+
+int main()
+{
+	PLL_init();
+	PWM_init();
+	IO0DIR=0x00;
+	while(1)
+	{
+			PWMMR5=20000; 
+			PWMLER=(1<<5);
+	}
+
+}
+
+void PLL_init()
+{
+	PLL0CON=0x01;
+	PLL0CFG=0x24;
+	PLL0FEED=0xAA;
+	PLL0FEED=0x55;
+	while((PLL0STAT & (1<<10))==0);
+	PLL0CON=0x02;
+	PLL0FEED=0xAA;
+	PLL0FEED=0x55;
+	VPBDIV=0x01;
+}
+void PWM_init()
+{
+	  PINSEL1=0x0400;
+	  PWMMR0=60000;
+	  PWMTCR=0x09;
+	  PWMMCR=0x02;
+	  PWMPR=0;
+	  PWMPCR=(1<<13);
+}
