8a62b05acd5659159d890d84d25814854a6d926b
Aditya_Puri <101449469+Adi1042003@users.noreply.github.com>
2023-07-29 15:24:25 +0530

    Create 4khz60%&40%.c
    2. Generate a 4KHz wave with 60% duty cycle and also for 40 % duty cycle.

---
+++ 4khz60%&40%.c
@@ -0,0 +1,41 @@
+//2. Generate a 4KHz wave with 60% duty cycle and also for 40 % duty cycle.
+
+#include"lpc214x.h"
+
+void PLL_init(void);
+void PWM_init(void);
+
+int main()
+{
+	PLL_init();
+	PWM_init();
+	while(1)
+	{
+		PWMMR1=9000;
+		PWMMR2=6000;
+		PWMLER=0x06;
+	}
+
+}
+
+void PLL_init()
+{
+	PLL0CON=0x01;
+	PLL0CFG=0x24;
+	PLL0FEED=0xAA;
+	PLL0FEED=0x55;
+	while((PLL0STAT & (1<<10))==0);
+	PLL0CON=0x02;
+	PLL0FEED=0xAA;
+	PLL0FEED=0x55;
+	VPBDIV=0x01;
+}
+void PWM_init()
+{
+	  PINSEL0=0x00008002;
+	  PWMMR0=15000;
+	  PWMTCR=0x09;
+	  PWMMCR=0x02;
+	  PWMPR=0;
+	  PWMPCR=0x0600;
+}
