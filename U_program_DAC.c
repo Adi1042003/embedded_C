5c82394f9eaf63b7d21b40c5295fd0655703be46
Aditya_Puri <101449469+Adi1042003@users.noreply.github.com>
2023-08-15 02:01:17 +0530

    Create U_program_DAC.c

---
+++ U_program_DAC.c
@@ -0,0 +1,34 @@
+//__U__ program generation using DAC
+
+
+#include <lpc214x.h>
+#include "math.h"
+#define pi 3.142
+
+int delay(unsigned int time) {
+    int x, y;
+    for(x = 0; x <= time; x++)
+        for(y = 0; y <= 1275; y++);
+    return 0;
+}
+
+int main(void) {
+    int i;
+    volatile double X;
+	int dacValue;
+    PINSEL1 = (1 << 19);
+    IO0DIR = (1 << 25);
+    
+    while(1) {
+		
+        for(i = 512; i <= 1024; i++) 
+		{
+            X = sin(i * 2 * pi / 1024); 
+            dacValue = (int)((X + 1.0) * 512); 
+            DACR = (dacValue << 6);
+        }
+		DACR=(0<<6);
+		delay(50);
+    }
+    return 0;
+}
