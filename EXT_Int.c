089b0f7f71510908c5d6156ba019999d824b21e6
Aditya_Puri <101449469+Adi1042003@users.noreply.github.com>
2023-09-11 14:59:09 +0530

    Create EXT_Int.c

---
+++ EXT_Int.c
@@ -0,0 +1,34 @@
+#include<lpc214x.h>
+int i,j;
+void delay(int time)
+{
+	for(i=0;i<time;i++)
+	for(j=0;j<120;j++);
+}
+__irq void Ext_ISR(void)
+{
+	long temp;
+	temp = EXTINT;
+	IO0PIN=~IO0PIN;
+	delay(10);
+	EXTINT = temp;
+	VICVectAddr= 0x00;
+}
+void init_ext(void)
+{
+	PINSEL0 |= (0x00<<16)|(0x03<<2);
+	IO0DIR = (0xFF<<16)|(0<<1);
+	EXTMODE=0x01;
+	EXTPOLAR=0x00;
+	VICVectAddr0=(unsigned int) Ext_ISR;
+	VICVectCntl0=(1<<5)|14;
+	VICIntSelect=0x0;
+	VICIntEnable =(1<<14);
+}
+int main()
+{
+  init_ext();
+  while(1)
+  {
+  }
+}
